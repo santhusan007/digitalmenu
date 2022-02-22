ba0b64ce71a787f4e3bd8e0cf726a87c124c2ed8
santhosh <511santhosh@gmail.com>
2022-02-22 14:01:37 +0530

    updated static

--- digitalmenu/settings.py
+++ digitalmenu/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 MEDIA_ROOT=os.path.join(BASE_DIR,'staticfiles')
-STATIC_URL = 'static/'
+STATIC_URL = '/gitstatic/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
