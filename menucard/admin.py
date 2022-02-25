ec020da029a3b64c18f4c036c5ae606f0c56d429
santhosh <511santhosh@gmail.com>
2022-02-25 21:23:16 +0530

    beautifiaction

--- menucard/admin.py
+++ menucard/admin.py
@@ -3,5 +3,13 @@ from .models import Item,Category
 
 # Register your models here.
 
-admin.site.register(Category) 
-admin.site.register(Item) 
+#admin.site.register(Category) 
+#admin.site.register(Item) 
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+   list_display=['title','created_by']
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+   list_display=['title','description','price','created_by']
